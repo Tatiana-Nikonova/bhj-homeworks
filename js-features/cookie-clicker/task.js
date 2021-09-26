6e0db6ab5033953c6267516ad1fe68735c02737c
Tatiana Nikonova <taniushka_2007@mail.ru>
2021-09-26 15:47:20 +0300

    Заливка задания с печенькой

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,19 @@
+const cookie = document.getElementById("cookie");
+const count = document.getElementById("clicker__counter");
+const clickSpeed = document.getElementById("click_speed");
+let start = Date.now();
+
+cookie.onclick = function () {
+	let end = Date.now();
+	let elapsed = end - start;
+	console.log(elapsed);
+
+	count.textContent++;
+	clickSpeed.textContent = (1 / (elapsed / 1000)).toFixed(2);
+
+	if (count.textContent % 2 !== 0) {
+		cookie.width = 300;
+	} else {
+		cookie.width = 200;
+	};
+};
