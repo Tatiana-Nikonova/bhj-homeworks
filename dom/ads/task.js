24cf189234a25bdfa1d1c9b080108dfe80a992b8
Tatiana Nikonova <taniushka_2007@mail.ru>
2021-10-13 14:39:38 +0300

    Доделала все задания

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,21 +1,22 @@
 const rotators = document.querySelectorAll(".rotator__case");
 
 function showRotator() {
+
 	for (let i = 0; i < rotators.length; i++) {
 
 		if (rotators.item(i).classList.contains("rotator__case_active")) {
-			rotators.item(i).classList.remove("rotator__case_active");
-		};
+			let speed = rotators.item(i).getAttribute("data-speed");
+			let color = rotators.item(i).getAttribute("data-color");
+			let index = 0;
+			rotators.item(i).style.color = color;
+			setTimeout(() => {
+				rotators.item(i).classList.remove("rotator__case_active");
+				if (i + 1 < rotators.length) index = (i + 1);
+				rotators.item(index).classList.add("rotator__case_active");
+				showRotator();
+			}, speed);
+		}
+	}
+}
 
-		let next = rotators.item(i).nextElementSibling;
-
-
-		if (next === rotators.item(i).lastElementChild) {
-			rotators.item(0).classList.add("rotator__case_active");
-		} else {
-			next.classList.add("rotator__case_active");
-		};
-	};
-};
-
-setInterval(showRotator, 1000);
+showRotator();
--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,28 +1,80 @@
-const fontSize = document.querySelectorAll(".font-size");
-const book = document.getElementById("book");
+let book = document.getElementById("book");
+let items = document.getElementsByClassName("font-size");
+let colors = document.getElementsByClassName("color");
+let content = document.querySelector(".book__content");
+const active = "font-size_active";
 
+for (let color of colors) {
+	color.onclick = () => {
+		let attribute = color.getAttribute("data-text-color");
+		if (attribute) {
+			for (let item of colors) {
+				if (item.getAttribute("data-text-color")) item.classList.remove("color_active");
+			}
+			color.classList.add("color_active");
+			if (attribute === 'whitesmoke') {
+				content.classList.remove("book_color-black");
+				content.classList.remove("book_color-gray");
+				content.classList.add("book_color-whitesmoke");
+			} else if (attribute === 'black') {
+				content.classList.remove("book_color-whitesmoke");
+				content.classList.remove("book_color-gray");
+				content.classList.add("book_color-black");
+			} else {
+				content.classList.remove("book_color-black");
+				content.classList.remove("book_color-whitesmoke");
+				content.classList.add("book_color-gray");
+			}
+		} else {
+			let attribute = color.getAttribute("data-bg-color");
+			if (attribute) {
+				for (let item of colors) {
+					if (item.getAttribute("data-bg-color")) item.classList.remove("color_active");
+				}
+				color.classList.add("color_active");
+				if (attribute === 'white') {
+					content.classList.remove("bg_color_gray");
+					content.classList.remove("bg_color_black");
+					content.classList.add("bg_color_white");
+				} else if (attribute === 'black') {
+					content.classList.remove("bg_color_gray");
+					content.classList.remove("bg_color_white");
+					content.classList.add("bg_color_black");
+				} else {
+					content.classList.remove("bg_color_white");
+					content.classList.remove("bg_color_black");
+					content.classList.add("bg_color_gray");
+				}
 
-
-for (const font of fontSize) {
-
-	function fontClick() {
-
-		let active = document.querySelector(".font-size_active");
-
-		if (active) {
-			active.classList.remove("font-size_active");
-		};
-
-		font.classList.add("font-size_active");
-
-		if (font.classList.contains("font-size_small")) {
-			book.className = "book book_fs-small";
-		} else if (font.classList.contains("font-size_big")) {
-			book.className = "book book_fs-big";
-		};
-
+			}
+		}
 		return false;
-	};
+	}
+}
 
-	font.addEventListener("click", fontClick);
-};
+for (let item of items) {
+	item.onclick = () => {
+		if (item.getAttribute("data-size") === "small") {
+			deleteClass();
+			item.classList.add(active);
+			book.classList.remove("font-size_big");
+			book.classList.add("font-size_small");
+		} else if (item.getAttribute("data-size") === "big") {
+			deleteClass();
+			item.classList.add(active);
+			book.classList.remove("font-size_small");
+			book.classList.add("font-size_big");
+		} else {
+			deleteClass();
+			item.classList.add(active);
+			book.classList.remove("font-size_big");
+			book.classList.remove("font-size_small");
+		}
+		return false;
+	}
+}
+
+function deleteClass() {
+	let element = document.querySelector(".font-size_active");
+	element.classList.remove("font-size_active");
+}
